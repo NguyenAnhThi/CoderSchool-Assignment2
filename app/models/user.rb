fe96fd807885caa95257567b68d1f4c21e108bc3
Nguyen Anh Thi <nguyenanhthi@gogojungle.vn>
2017-02-24 22:19:16 +0700

    User can sign up by providing their email, password, and name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,4 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
-
-  def password=(value)
-    self.password_digest = Encrypter.encrypt(value)
-  end
 end
