4de0243257e41ee14b3416399f7b6560cc15b73f
Nguyen Anh Thi <nguyenanhthi@gogojungle.vn>
2017-02-26 16:34:29 +0700

    add inverse_friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,8 @@
 class FriendshipsController < ApplicationController
   def create
     @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-    if @friendship.save
+    @inverse_friendship = User.find(params[:friend_id]).friendships.build(friend_id: current_user.id)
+    if @friendship.save and @inverse_friendship.save
       flash[:success] = "Added friend!"
       redirect_to users_path
     else
@@ -12,7 +13,9 @@ class FriendshipsController < ApplicationController
 
   def destroy
     @friendship = current_user.friendships.where(user_id: current_user, friend_id: params[:friend_id])
+    @inverse_friendship = Friendship.find_by(user_id: params[:friend_id], friend_id: current_user.id)
     current_user.friendships.destroy(@friendship)
+    User.find(params[:friend_id]).friendships.destroy(@inverse_friendship)
     flash[:success] = "Removed friendship!"
     redirect_to user_path
   end
